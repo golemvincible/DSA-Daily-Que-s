51d4602c89962bd05fa0fa8148e9479f478fedd9
Sourabh Kothari <jainjain390@gmail.com>
2023-04-14 04:04:18 +0530

    day 2 completed

---
+++ recursion/day 2/Subsets1.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//https://leetcode.com/problems/subsets/
+
+public class Subsets1 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        System.out.println(subsets(arr));
+    }
+
+    private static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> op = new ArrayList<>();
+
+        helperSubsets(nums,op,res,0);
+
+        return res;
+    }
+
+    private static void helperSubsets(int[] nums, List<Integer> op, List<List<Integer>> res, int start) {
+        res.add(new ArrayList<>(op));
+
+        for(int i=start;i<nums.length;i++){
+            op.add(nums[i]);
+            helperSubsets(nums,op,res,i+1);
+            op.remove(op.size()-1);
+        }
+    }    
+}
---
+++ recursion/day 2/Subsets2.java
@@ -0,0 +1,47 @@
+//https://leetcode.com/problems/subsets-ii/
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Subsets2 {
+    private static void helpersubsetsWithDup(int[] nums,List<Integer> op,List<List<Integer>> res,int start){
+
+        res.add(new ArrayList<>(op));
+
+        for(int i=start;i<nums.length;i++){
+            /*
+                we have included start element thats why we will start checking
+                from next element of start
+                if the next ele is equal to prev we will skip
+                no further recursion calls for them as they will generate
+                same answers
+            */
+            if(i>start && nums[i]==nums[i-1])
+                continue;
+
+            op.add(nums[i]);
+            helpersubsetsWithDup(nums,op,res,i+1);
+            op.remove(op.size()-1);
+        }
+
+    }
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> op = new ArrayList<>();
+        /*
+            because we are dealing with positions so we want to skip duplicates
+            which can only be done if they all come together thats why we are sorting
+            the array
+        */
+        Arrays.sort(nums);
+        helpersubsetsWithDup(nums,op,res,0);
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,2,2};
+        System.out.println(subsetsWithDup(arr));
+    }
+}
+
