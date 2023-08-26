f192a7fdfc66443fd09c9cad137adb71c9a7609e
Sourabh Kothari <jainjain390@gmail.com>
2023-08-26 09:56:55 +0530

    P3 using dfs

--- O6Graph/day2/CourseSchedule.java
+++ O6Graph/day2/CourseSchedule.java
@@ -5,6 +5,21 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class CourseSchedule {
+    private static boolean dfs(ArrayList<ArrayList<Integer>> adj,int v,boolean[] vis,boolean[] pathVis,int node){
+        vis[node]=true;
+        pathVis[node]=true;
+        
+        for(Integer it:adj.get(node)){
+            if(!vis[it]){
+                if(dfs(adj,v,vis,pathVis,it))
+                    return true;
+            }
+            else if(vis[it] && pathVis[it])
+                return true;
+        }
+        pathVis[node] = false;
+        return false;
+    }
     private static ArrayList<Integer> kahnsAlgo(ArrayList<ArrayList<Integer>> adj,int v){
         ArrayList<Integer> res = new ArrayList<>();
         int[] indegree = new int[v];
@@ -46,6 +61,16 @@ public class CourseSchedule {
             adj.get(arr[0]).add(arr[1]);
         }
         
-        return v==kahnsAlgo(adj,v).size();
+        // return v==kahnsAlgo(adj,v).size();
+        boolean[] vis = new boolean[v];
+        boolean[] pathVis = new boolean[v];
+        
+        for(int i=0;i<v;i++){
+            if(!vis[i]){
+                if(dfs(adj,v,vis,pathVis,i))
+                    return false;
+            }
+        }
+        return true;
     }
 }
