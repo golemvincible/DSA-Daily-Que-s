4f46eec58a4ae1204cd1a981b5ef0fc4c925a19c
Sourabh Kothari <jainjain390@gmail.com>
2023-08-26 09:19:01 +0530

    P1 by bfs

--- O6Graph/day2/TopologicalSort.java
+++ O6Graph/day2/TopologicalSort.java
@@ -1,9 +1,42 @@
 package O6Graph.day2;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class TopologicalSort{
+    //bfs is this
+    private static int[] kahnsAlgo(ArrayList<ArrayList<Integer>> adj,int v){
+        int[] res = new int[v];
+        int[] indegree = new int[v];
+        
+        Queue<Integer> q = new LinkedList<>();
+        
+        //filling indegree
+        for(int i=0;i<v;i++){
+            for(Integer it:adj.get(i)){
+                indegree[it]++;
+            }
+        }
+        //initializing q
+        for(int i=0;i<v;i++){
+            if(indegree[i]==0)  q.offer(i);
+        }
+        int i=0;
+        while(!q.isEmpty()){
+            int n = q.poll();
+            res[i++] = n;
+            //reducing indegree of its adj nodes
+            for(Integer it:adj.get(n)){
+                indegree[it]--;
+                if(indegree[it]==0)
+                    q.offer(it);
+            }
+        }
+        
+        return res;
+    }
     private static void dfs(ArrayList<ArrayList<Integer>> adj,int v,Stack<Integer> st,int node,boolean[] vis){
         vis[node] = true;
 
